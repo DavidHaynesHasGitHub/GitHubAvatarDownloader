48bf99ffc7431bfa4e51b0326ed56ca0d35d9805
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-19 19:37:33 -0800

    now prints the avatar urls

--- download_avatars.js
+++ download_avatars.js
@@ -3,33 +3,26 @@ var secret = require('./secret');
 var fs = require('fs');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+var avaURL = ''
 
-function getRepoContributors(repoOwner, repoName, cb) {
-    var options ={
+function getRepoContributors(repoOwner, repoName, callback) {
+    var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
-     'User-Agent': 'request',
+     'User-Agent': 'DavidHaynesHasGitHub',
      'Authorization' : secret
-   }
+      }
     };
+
   request(options, function(err, res, body) {
-    cb(err, body);
+    callback(err, body);
+      var avaURL = JSON.parse(body)
+      for (element of avaURL)
+      console.log(element['avatar_url'])
   });
 }
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  
-  var avaURL = JSON.parse(result)
 
-  console.log(avaURL[0]['avatar_url'])
 
-  for (let element of avaURL) {
-    request.get(element['avatar_url'])               // Note 1
-           .on('error', function (err) {                                   // Note 2
-             throw err;
-           })
-           .on('response', function (response) {                           // Note 3
-             console.log('Response Status Code: ', response.statusCode);
-           })
-           .pipe(fs.createWriteStream("./avatars/" + element["login"] + '.png'))  }
+getRepoContributors("jquery", "jquery", function(err, result){
+
 });
