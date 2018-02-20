216ba8793bd30b9bfc68a6d51c89a187326602b0
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-19 17:03:27 -0800

    started the download_avatar.js, takes in the jquery api

---
+++ download_avatars.js
@@ -0,0 +1,9 @@
+var request = require('request');
+
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
+}
