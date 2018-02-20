f27e16aef744bc2059f9f5c54b5dc6e9b56caeb6
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-19 17:14:50 -0800

    added github token

--- download_avatars.js
+++ download_avatars.js
@@ -1,9 +1,21 @@
 var request = require('request');
 
+console.log('Welcome to the GitHub Avatar Downloader!');
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+  var options ={
+     url : "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+     headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options.url, function(err, res, body) {
     cb(err, body);
   });
 }
