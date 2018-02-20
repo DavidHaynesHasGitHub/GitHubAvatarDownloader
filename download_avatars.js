1f850528e0ae8c61ffeefaaf7db7f1c010666642
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-20 10:09:28 -0800

    improved the comments

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,7 @@ var secret = require('./secret');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 
-
+// first function that gets called, gets the list of contributors
 function getRepoContributors(repoOwner, repoName, callback) {
     var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
@@ -22,17 +22,8 @@ function getRepoContributors(repoOwner, repoName, callback) {
      });
 }
 
-function downloadImageByURL(url, filepath) {
-    request.get(url, function(err, res, body){
-        if(err) {
-            throw err;
-        }
-        console.log('Downloading...' + url);
-    })
-    .pipe(fs.createWriteStream(filepath));
-}
-
-
+// callback function throws error if unable to get data due to passed info
+// arses the json file and passes the avatar url to downloadImageByURL
 var callback = function (error, response, body) {
     if (!error && response.statusCode == 200) {
         mkdirp('./avatars', function (err) {
@@ -50,4 +41,16 @@ var callback = function (error, response, body) {
     }
 }
 
+//takes the passed url from the callback function and GET the image and downloads it
+function downloadImageByURL(url, filepath) {
+    request.get(url, function(err, res, body){
+        if(err) {
+            throw err;
+        }
+        console.log('Downloading...' + url);
+    })
+    .pipe(fs.createWriteStream(filepath));
+}
+
+//calls the function bytakes repo name and user name and passes it to the function
 getRepoContributors(process.argv[2], process.argv[3], callback)
