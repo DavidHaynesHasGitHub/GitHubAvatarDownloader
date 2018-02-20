da7f96ccea2879f16985610699768d22811ad1bb
David Haynes <DavidHaynesVictoria@gmail.com>
2018-02-19 23:23:11 -0800

    have code working

--- download_avatars.js
+++ download_avatars.js
@@ -1,37 +1,53 @@
 var request = require('request');
 var secret = require('./secret');
 var fs = require('fs');
-
-console.log('Welcome to the GitHub Avatar Downloader!');
-var avaURL = ''
+var mkdirp = require('mkdirp');
 
 function getRepoContributors(repoOwner, repoName, callback) {
     var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
       headers: {
      'User-Agent': 'DavidHaynesHasGitHub',
-     'Authorization' : secret
+     'Authorization' : 'token ' + secret.GITHUB_TOKEN
       }
     };
 
-  request(options, function(err, res, body) {
-    callback(err, body);
-      var avaURL = JSON.parse(body)
-
-  });
+  request(options, function (err, res, body) {
+    if (err){
+      throw err
+    }
+    var avaURL = JSON.parse(body)
+         callback(err, res, body);
+     });
 }
-getRepoContributors("jquery", "jquery", function(err, result){
-
-});
 
 function downloadImageByURL(url, filepath) {
     request.get(url, function(err, res, body){
         if(err) {
             throw err;
         }
-        console.log("Downloading..." + url);
+        console.log('Downloading...' + url);
     })
     .pipe(fs.createWriteStream(filepath));
 }
 
-downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
+
+var callback = function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+        mkdirp('./avatars', function (err) {
+            console.log('Directory created');
+        });
+        var filepath;
+        var info = JSON.parse(body);
+        for (let element of info) {
+            filepath = './avatars/' + element['login'] + '.png';
+            downloadImageByURL(element['avatar_url'], filepath)
+        }
+    } else {
+        console.log('ya dun fucked it good')
+        throw err;
+    }
+}
+var repoOwner = 'jQuery'
+var repoName = 'jQuery'
+getRepoContributors(repoOwner, repoName, callback)
